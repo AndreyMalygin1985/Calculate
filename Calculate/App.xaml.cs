115ab3d50a230828cd22146824c78682c9e360ed
BW321 <kunticha09@gmail.com>
2024-03-21 21:19:53 +0300

    21:20

--- Calculate/App.xaml.cs
+++ Calculate/App.xaml.cs
@@ -11,7 +11,16 @@ namespace Calculate
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
-    public partial class App : Application
+   /* public partial class App : Application
     {
-    }
+        public App()
+        {
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            var 
+        }
+    }*/
 }
